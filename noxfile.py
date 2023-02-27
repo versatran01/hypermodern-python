c7f5f53b5a6d1e9a87a232edea3cd9ffd6a66513
chao <quchao@seas.upenn.edu>
2023-02-26 19:56:49 -0800

    fix install_with_constraints

--- noxfile.py
+++ noxfile.py
@@ -1,24 +1,42 @@
-import nox
 import tempfile
 
-# Exclude black from the sessions run by default
-nox.options.sessions = "lint", "safety", "tests"
+import nox
 
+# Exclude black from the sessions run by default
+nox.options.sessions = "lint", "tests", "mypy"
+
+package = "hypermodern_python"
 locations = "src", "tests", "noxfile.py"
 python_versions = ["3.10"]
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=constraints.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=python_versions)
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "coverage", "pytest", "pytest-cov", "pytest-mock")
     session.run("pytest", *args)
 
 
 @nox.session(python=python_versions)
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -31,7 +49,7 @@ def lint(session):
 @nox.session(python=python_versions)
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -49,3 +67,10 @@ def safety(session):
         )
         session.install("safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=python_versions)
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
