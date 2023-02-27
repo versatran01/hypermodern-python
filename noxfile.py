c4b8586409b2cac14d8ccff1be172b4fa1a3bee9
chao <quchao@seas.upenn.edu>
2023-02-26 17:21:03 -0800

    add safety

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,8 @@
 import nox
+import tempfile
 
 # Exclude black from the sessions run by default
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 
 locations = "src", "tests", "noxfile.py"
 python_versions = ["3.10"]
@@ -32,3 +33,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python=python_versions)
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
